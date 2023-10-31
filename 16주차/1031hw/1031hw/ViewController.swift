da42f2d8a7779b3c6ab9c8b0ef3ea2d374a0125c
limsub <limsss9076@naver.com>
2023-10-31 19:48:47 +0900

    10/31 hw start

---
+++ 16주차/1031hw/1031hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1031hw
+//
+//  Created by 임승섭 on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
